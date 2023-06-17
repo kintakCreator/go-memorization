70447c0f0401fe6697aca111849e53405fd97e22
kintakCreator <kinas5727@gmail.com>
2023-06-17 17:01:42 +0300

    Service layer and some tests

---
+++ account/service/user_service.go
@@ -0,0 +1,34 @@
+package service
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/jacobsgoodwin/memrizr/model"
+)
+
+// UserService acts as a struct for injecting an implementation of UserRepository
+// for use in service methods
+type UserService struct {
+	UserRepository model.UserRepository
+}
+
+// USConfig will hold repositories that will eventually be injected into this
+// service layer
+type USConfig struct {
+	UserRepository model.UserRepository
+}
+
+// NewUserService is a factory function for
+// init a UserService with its repository layer dependencies
+func NewUserService(c *USConfig) model.UserService {
+	return &UserService{
+		UserRepository: c.UserRepository,
+	}
+}
+
+// Get retrieves a user based on their uuid
+func (s *UserService) Get(ctx context.Context, uid uuid.UUID) (*model.User, error) {
+	u, err := s.UserRepository.FindByID(ctx, uid)
+	return u, err
+}
